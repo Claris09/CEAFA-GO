0f5bf1748b3b72b8c217ef161934726e0e277111
Jay-Me871 <jaimiemelanie16@gmail.com>
2022-11-30 10:24:33 +0800

    menus

--- ceafana-20221003T035749Z-001/ceafana/Assets/MainMenu.cs
+++ ceafana-20221003T035749Z-001/ceafana/Assets/MainMenu.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-  public void ExitButton(){
-        Application.Quit();
-        Debug.Log("Game closed");
-    }
+  
   public void StartGame(){
         SceneManager.LoadScene("SampleScene");
+    }
+
+  public void ExitButton(){
+        
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
 }
